f78414604ec9370bf2ff69bfad0b48aa4879b815
aneeshk <aneeshk@cubettech.com>
2015-12-30 12:37:53 +0530

    Doc updated

--- site/public/apidoc/api_project.js
+++ site/public/apidoc/api_project.js
@@ -24,7 +24,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-12-30T06:32:32.259Z",
+    "time": "2015-12-30T07:07:16.819Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
