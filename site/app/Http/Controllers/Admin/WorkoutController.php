acc95828f28d1685e4f4e5123ef2161913f6ba63
ansa <ansa@cubettech.com>
2016-01-05 17:32:25 +0530

    Added Workout controller for Admin. Added route for updating device token

---
+++ site/app/Http/Controllers/Admin/WorkoutController.php
@@ -0,0 +1,248 @@
+<?php namespace App\Http\Controllers\Admin;
+
+use Auth,
+    Hash,
+    DB;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use JWTAuth;
+use Redirect;
+use Input;
+use Lang;
+use Validator;
+use App\Workout;
+use App\Exercise;
+use App\Profile;
+use App\User;
+
+class WorkoutController extends Controller
+{
+
+    /**
+     * Index page
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function getIndex()
+    {
+        // Grab all the users
+        $exercise = Exercise::get();
+        $user = User::where('id', Auth::user()->id)->with(['profile', 'settings'])->first();
+        // Show the page
+        return View('admin.exercise.index', compact('exercise', 'user'));
+    }
+
+    /**
+     * User create form processing.
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function getCreate()
+    {
+        $user = User::where('id', Auth::user()->id)->with(['profile', 'settings'])->first();
+        // Show the page
+        return View('admin.exercise.create', compact('user'));
+    }
+
+    /**
+     * User create form processing.
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function postCreate()
+    {
+        try {
+
+            // Register the user
+           Exercise::create([
+                'name' => Input::get('name'),
+                'description' => Input::get('description'),
+                'category' => Input::get('category'),
+                'type' => Input::get('type'),
+                'rewards' => Input::get('rewards'),
+                'repetitions' => Input::get('repetitions'),
+                'duration' => Input::get('duration'),
+                'unit' => Input::get('unit'),
+                'equipment' => Input::get('equipment')
+            ]);
+       
+            //check for activation and send activation mail if not activated by default
+            // Redirect to the home page with success menu
+            return Redirect::route("exercise")->with('success', Lang::get('Successfully created'));
+        } catch (LoginRequiredException $e) {
+            $error = Lang::get('admin/users/message.user_login_required');
+        } catch (PasswordRequiredException $e) {
+            $error = Lang::get('admin/users/message.user_password_required');
+        } catch (UserExistsException $e) {
+            $error = Lang::get('admin/users/message.user_exists');
+        }
+
+        // Redirect to the user creation page
+        return Redirect::back()->withInput()->with('error', $error);
+    }
+
+    /**
+     * View page
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function show($id)
+    {
+
+        try {
+            //echo Auth::user()->id;die;
+            // Get the user information
+            $exercise = Exercise::where('id', $id)->first();
+            $user = User::where('id', Auth::user()->id)->with(['profile', 'settings'])->first();
+            $usersList = '';
+//        
+        } catch (UserNotFoundException $e) {
+            // Prepare the error message
+            $error = Lang::get('users/message.user_not_found', compact('id'));
+
+            // Redirect to the user management page
+            return Redirect::route('users')->with('error', $error);
+        }
+        // Show the page
+        return View('admin.exercise.show', compact('exercise', 'user', 'usersList'));
+    }
+
+    /**
+     * User Edit page
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function getEdit($id = null)
+    {
+        // Get the user information
+        if ($tUser = User::where('id', $id)->with(['profile', 'settings'])->first()) {
+            $user = User::where('id', Auth::user()->id)->with(['profile', 'settings'])->first();
+        } else {
+            // Prepare the error message
+            $error = Lang::get('users/message.user_not_found', compact('id'));
+
+            // Redirect to the user management page
+            return Redirect::route('users')->with('error', $error);
+        }
+
+        // Show the page
+        return View('admin/users/edit', compact('user', 'tUser'));
+    }
+
+    /**
+     * User Post Edit page
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function postEdit($id = null)
+    {
+        try {
+            // Get the user information
+            $user = User::where('id', $id)->first();
+        } catch (UserNotFoundException $e) {
+            // Prepare the error message
+            $error = Lang::get('users/message.user_not_found', compact('id'));
+
+            // Redirect to the user management page
+            return Redirect::route('users')->with('error', $error);
+        }
+
+        //
+        $this->validationRules['email'] = "required|email|unique:users,email,{$user->email},email";
+
+        // Do we want to update the user password?
+        if (!$password = Input::get('password')) {
+            unset($this->validationRules['password']);
+            unset($this->validationRules['password_confirm']);
+        }
+
+        // Create a new validator instance from our validation rules
+        $validator = Validator::make(Input::all(), $this->validationRules);
+
+        // If validation fails, we'll exit the operation now.
+        if ($validator->fails()) {
+            // Ooops.. something went wrong
+            return Redirect::back()->withInput()->withErrors($validator);
+        }
+
+        try {
+
+            $userProfile = Profile::where('user_id', $id)->first();
+            // Update the user
+            $userProfile->first_name = Input::get('first_name');
+            $userProfile->last_name = Input::get('last_name');
+            $userProfile->gender = Input::get('gender');
+            $userProfile->country = Input::get('country');
+            $userProfile->state = Input::get('state');
+            $userProfile->city = Input::get('city');
+            $userProfile->spot = Input::get('spot');
+            $userProfile->quote = Input::get('quote');
+            $userProfile->save();
+
+            // Was the user updated?
+            if ($user->save()) {
+                // Prepare the success message
+                $success = Lang::get('users/message.success.update');
+                // Redirect to the user page
+                return Redirect::route('users.update', $id)->with('success', $success);
+            }
+            // Prepare the error message
+            $error = Lang::get('users/message.error.update');
+        } catch (LoginRequiredException $e) {
+            $error = Lang::get('users/message.user_login_required');
+        }
+
+        // Redirect to the user page
+        return Redirect::route('users.update', $id)->withInput()->with('error', $error);
+    }
+
+    /**
+     * User Delete
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function getDeletedUsers()
+    {
+        // Grab deleted users
+        $users = User::onlyTrashed()->get();
+
+        // Show the page
+        return View('admin/deleted_users', compact('users'));
+    }
+
+    /**
+     * User Delete
+     * @since 04/01/2015
+     * @author ansa@cubettech.com
+     * @return json
+     */
+    public function getModalDelete($id = null)
+    {
+        $model = 'users';
+        $confirm_route = $error = null;
+        try { //echo 'hiii';die;
+            // Get user information
+            // $user = User::where('id', $id)->first();
+            // Check if we are not trying to delete ourselves
+            //  if ($user->id === Sentinel::getUser()->id) {
+            // Prepare the error message
+            //  $error = Lang::get('users/message.error.delete');
+            return View('admin.layouts.modal_confirmation', compact('model', 'confirm_route'));
+            // }
+        } catch (UserNotFoundException $e) {
+            // Prepare the error message
+            $error = Lang::get('users/message.user_not_found', compact('id'));
+            return View('admin/layouts/modal_confirmation', compact('error', 'model', 'confirm_route'));
+        }
+        $confirm_route = route('delete/user', ['id' => $user->id]);
+        return View('admin/layouts/modal_confirmation', compact('error', 'model', 'confirm_route'));
+    }
+}
