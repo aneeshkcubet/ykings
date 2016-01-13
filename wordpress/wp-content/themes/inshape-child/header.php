a52bc430a60ac1f61086882a9ff47c8d74928934
chinthu <chinthu.santhosh@cubettech.com>
2016-01-13 17:23:36 +0530

    facebook pixel added

--- wordpress/wp-content/themes/inshape-child/header.php
+++ wordpress/wp-content/themes/inshape-child/header.php
@@ -20,6 +20,20 @@
     //the_title('YKings ');?>
     </title>
     <?php tfuse_meta(); ?>
+        <!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '518896258281332');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=518896258281332&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php
@@ -31,6 +45,9 @@
         wp_head();
     ?>
 </head>
+<script type="text/javascript">
+fbq('track', 'ViewContent');
+</script>
 <body <?php body_class();?>>
     <div id="page" class="hfeed site">
         <header id="masthead" class="site-header">
